1a2a6c685d94f04fed3f93f4fdc15fe930c161c3
Bruno Petrus <bruno.petrus@live.com>
2016-07-27 20:05:59 +0200

    Finished. Only documentation remains. Also change from directlyrics to something else

--- src/MainController.java
+++ src/MainController.java
@@ -3,6 +3,9 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -28,6 +31,36 @@ public class MainController implements Initializable{
     }
 
     public void search() {
+        try {
+            String base = "http://www.directlyrics.com/";
+            String title = tf_title.getText(); title = title.trim(); title = title.replaceAll(" ", "-");
+            String artist = tf_artist.getText(); artist = artist.trim(); artist = artist.replaceAll(" ", "-");
+            base += artist + "-" + title + "-lyrics.html";
+            System.out.println(base);
 
+            Document doc = Jsoup.connect(base).get();
+            Elements d = doc.getElementsByAttributeValue("class", "lyrics lyricsselect");
+            String lyrics = d.toString();
+
+            int firstP = lyrics.indexOf("<p>");
+            int secondP = lyrics.indexOf("</p");
+            lyrics = lyrics.substring(firstP + 3, secondP);
+            lyrics = lyrics.replace("<br>", "\n");
+            while(true) {
+                firstP = lyrics.indexOf("<ins");
+                secondP = lyrics.indexOf("</script>");
+                if (firstP == -1 || secondP == -1)
+                    break;
+                String lyricsFirstPart = lyrics.substring(0, firstP);
+                String lyricsLastPart = lyrics.substring(secondP+9);
+                lyrics = lyricsFirstPart+lyricsLastPart;
+            }
+
+
+            ta_lyrics.setText(lyrics);
+        }
+        catch(Exception e) {
+            e.printStackTrace();
+        }
     }
 }
